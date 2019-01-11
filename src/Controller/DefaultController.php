00cae1fa0ad793f1e2ba12ebdc6ac1d67b0f9948
greggallant <greggallant2005@gmail.com>
2019-01-11 17:38:45 -0500

    Configured template

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -7,6 +7,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use Psr\Log\LoggerInterface;
+
 class DefaultController extends AbstractController 
 {
   
@@ -14,91 +16,14 @@ class DefaultController extends AbstractController
     {
      
         $newvar = "100";
-     
-        return new Response(
-            '<html>
-                <body>
-                    <p><strong>Symfony Plan Controller</strong></p>
-                    <ul>
-                        <li> <a href="/struct">Router Tests</a></li>
-                        <li> <a href="/forms">Form Tests</a></li>
-                        <li> <a href="/login">Authentication Tests</a></li>
-                        <li> <a href="/cacheconfig">Caching Setups</a></li>
-                        <li> <a href="/nuxt">NuxtJS or Twig</a></li>
-                    </ul>
-                </body>
-             </html>'
-        ); 
+        return $this->render('base.html.twig');       
+             
     }
 
-    public function struct() 
+    public function docs(LoggerInterface $logger) 
     {
-        return new Response(
-            '<html>
-                <body>
-                    <strong>Controller structuring tests for Symfony</strong>
-                    <p> Test Yaml vs. Annotated Routes </p>
-                    Setup: <a href="https://symfony.com/doc/current/components/routing.html">https://symfony.com/doc/current/components/routing.html</a>
-                </body>
-             </html>'
-        ); 
-    } 
-
-
-    public function formtest() {
-        return new Response(
-            '<html>
-                <body>
-                    <strong>Form structuring</strong>
-                    <p> Templating and Twig Setup first </p>
-                    Templating: <a href="https://symfony.com/doc/current/templating.html">https://symfony.com/doc/current/templating.html</a>
-                    <br/>
-                    Doctrine ORM: <a href="https://symfony.com/doc/current/doctrine.html">Doctrine</a>
-                    <br/>
-                    Forms: <a href="https://symfony.com/doc/current/forms.html">Forms</a> 
-                </body>
-             </html>'
-        ); 
-
-    }
-
-    public function authtest() {
-        return new Response(
-            '<html>
-                <body>
-                    <strong>Form structuring</strong>
-                    <p> Security </p>
-                    <a href="https://symfony.com/doc/current/security.html">https://symfony.com/doc/current/security.html</a>
-                    <p> Login Form </p>
-                    <a href="https://symfony.com/doc/current/security/form_login.html">https://symfony.com/doc/current/security/form_login.html</a>
-                    <p> Login Form Setup</p>
-                    <a href="https://symfony.com/doc/current/security/form_login_setup.html">https://symfony.com/doc/current/security/form_login_setup.html</a>
-                    <p> Guard Authentication </p>
-                    <a href="https://symfony.com/doc/current/security/guard_authentication.html">https://symfony.com/doc/current/security/guard_authentication.html</a>
-                </body>
-            </html>
-            '
-        );
-    }
-
-    public function cachetest() {
-        return new Response(
-            '<html>
-                <body>
-                    <strong>Cache structuring</strong>
-                </body>
-            </html>'
-        );
-    }
-
-    public function nuxttest() {
-        return new Response(
-            '<html>
-                <body>
-                    <strong><a href="https://nuxtjs.org">NuxtJS</a></strong>
-                </body>
-            </html>'
-         );
+        $logger->info('Logging Tests');
+        return $this->render('docs.html.twig');       
     }
 
 }
